567b912188fe2a4a6ff685c9e1b085f458bd1321
ogeorge <ec2-user@ip-172-31-30-65.us-west-2.compute.internal>
2015-03-06 19:39:07 +0000

    First Commit

---
+++ Server.js
@@ -0,0 +1,97 @@
+var fs = require('fs');
+
+var http = require('http');
+
+var url = require('url');
+
+var ROOT_DIR = 'html/';
+
+
+
+http.createServer(function (req, res)
+
+{
+
+	var urlObj = url.parse(req.url, true, false);
+
+	
+
+	if(urlObj.pathname.indexOf('getcity') != -1)
+
+	{
+
+		fs.readFile('cities.dat.txt', function (err,data)
+
+		{
+
+			if(err) throw err;
+
+
+
+			var cities = data.toString().split('\n');
+
+			var myRegEx = RegExp('^'+urlObj.query['q']);
+
+			var jsonresult = []
+
+			
+
+			for(var i = 0; i < cities.length; i++)
+
+			{
+
+				var result = cities[i].search(myRegEx);
+
+				if(result != -1)
+
+				{
+
+					console.log(cities[i]);
+
+					jsonresult.push({city: cities[i]});
+
+				}
+
+			}
+
+			console.log(JSON.stringify(jsonresult));
+
+			res.writeHead(200);
+
+			res.end(JSON.stringify(jsonresult));
+
+
+
+		});
+
+	}
+
+	else
+
+	{
+
+		fs.readFile(ROOT_DIR + urlObj.pathname, function (err,data)
+
+		{ 
+
+			if (err) 
+
+			{
+
+				res.writeHead(404);
+
+				res.end(JSON.stringify(err));
+
+				return;
+
+			}
+
+			res.writeHead(200);
+
+			res.end(data);
+
+		});
+
+	}
+
+}).listen(80);
